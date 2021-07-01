7f66414dac67bf4b30b4f48961d0fcdd994a4bd2
mr200179 <mr200179@P007D006.reigate.ac.uk>
2021-07-01 10:59:45 +0100

    gui examples

---
+++ src/productivitybooster/ProductivityBooster.java
@@ -0,0 +1,20 @@
+
+package productivitybooster;
+
+import java.awt.Toolkit;
+
+
+public class ProductivityBooster {
+
+   
+    public static void main(String[] args) {
+          mainMenu start = new mainMenu(); 
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        int width =(int) tk.getScreenSize().getWidth();
+         int height =(int) tk.getScreenSize().getHeight();
+
+        start.setSize(width,height);
+        start.setVisible(true);
+    }
+    
+}
